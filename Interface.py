8dfd1c5c22bde6a18c35391867882a2a722d8fba
Brooks Adcock <git@brooksadcock.com>
2019-08-28 10:43:17 -0400

    web interface started

--- Interface.py
+++ Interface.py
@@ -1,10 +1,25 @@
-import Tkinter as tk
-tk._test()
+from flask import Flask, render_template, jsonify
+import math, json
 
-root = tk.Tk()
+app = Flask(__name__)
 
-w = tk.Canvas(root, width=400, height=300)
-w.pack()
+seconds_to_show = 3.0
+sample_frequency = 60.0*4.0
+t_ms = list(map(lambda x: float(x)/1000.0, range(int(seconds_to_show * sample_frequency))))
+y = list(map(lambda t: math.sin(t), t_ms))
+signal_data = list(map(lambda i: {'x': t_ms[i], 'y': y[i]}, range(int(seconds_to_show * sample_frequency))))
 
-root.mainloop()
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route("/signal")
+def signal():
+	return jsonify(signal_data)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
 
