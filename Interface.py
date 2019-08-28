c6cf077a83c1bad3ceaeb1e75ccc3f762522264d
Brooks Adcock <git@brooksadcock.com>
2019-08-28 17:15:19 -0400

    line is showing correctly but not dynamic

--- Interface.py
+++ Interface.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 seconds_to_show = 3.0
 sample_frequency = 60.0*4.0
-t_ms = list(map(lambda x: float(x)/1000.0, range(int(seconds_to_show * sample_frequency))))
-y = list(map(lambda t: math.sin(t), t_ms))
-signal_data = list(map(lambda i: {'x': t_ms[i], 'y': y[i]}, range(int(seconds_to_show * sample_frequency))))
+n_samples = int(seconds_to_show * sample_frequency)
+t_ms = list(map(lambda x: float(x)/sample_frequency, range(n_samples)))
+y = list(map(lambda t: math.sin(t*2*math.pi), t_ms))
+signal_data = list(map(lambda i: {'x': t_ms[i], 'y': y[i]}, range(n_samples)))
 
 
 @app.route('/')
