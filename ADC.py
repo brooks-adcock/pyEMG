d440f9adeba322678022eab3c4604d2b654e366a
Brooks Adcock <git@brooksadcock.com>
2019-09-06 22:15:10 -0400

    mcp3001 working

--- ADC.py
+++ ADC.py
@@ -1,37 +1,27 @@
-  
-# Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
 import time
+import spidev
 
-# Import SPI library (for hardware SPI) and MCP3008 library.
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
-
-
-# Software SPI configuration:
-#CLK  = 18
-#MISO = 23
-#MOSI = 24
-#CS   = 25
-#mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-
-# Hardware SPI configuration:
-
-SPI_PORT   = 0
+SPI_PORT = 0
 SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+SPI_SPEED = 1000000
+
+device = spidev.SpiDev()
+device.open(SPI_PORT, SPI_DEVICE)
+device.max_speed_hz = SPI_SPEED
+# Default to mode 0, and make sure CS is active low.
+device.mode = 0
+#device.cshigh = False
+#device.lsbfirst = False
+
 
 def getSample():
+	reading = bytearray(device.readbytes(2))
+	hi = reading[0] & 0x1f
+	lo = reading[1] & 0xf8
 
-    values = [0]*8
-    for i in range(8):
-        # The read_adc function will get the value of the specified channel (0-7).
-        values[i] = mcp.read_adc(i)
+	v_in = (hi << 5) + (lo >> 3)
+	VOLTAGE = 3.3
+	v_in = v_in*1.0/1024.0-0.5
+	v_in *= VOLTAGE
 
-    #sample on [-1.0, 1.0] scale
-    sample = float(values[0])/512.0 - 1.0
-    voltage = 3.3 / 2.0
-    return {'t': time.time(), 'y': voltage * sample}
-        
+	return {"t": time.time(), "y": v_in}
